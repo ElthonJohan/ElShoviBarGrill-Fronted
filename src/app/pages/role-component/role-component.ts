89ff92718bac8771708e637ffd04939efca02494
EduCulqui <eduardoculquisan80@gmail.com>
2025-11-08 22:09:00 -0500

    Add components and services

---
+++ src/app/pages/role-component/role-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-role-component',
+  imports: [],
+  templateUrl: './role-component.html',
+  styleUrl: './role-component.css',
+})
+export class RoleComponent {
+
+}
